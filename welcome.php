db71c54563239b732410f53cb912a8e883847d91
SG <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-07-12 14:32:47 +0530

    edit

--- welcome.php
+++ welcome.php
@@ -12,50 +12,7 @@ require_once "config.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UserAccount | Techsevin</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-        .sidepanel {
-            width: 0;
-            position: fixed;
-            z-index: 1;
-            height: 250px;
-            top: 0;
-            right: 0;
-            background-color: #111;
-            overflow-x: hidden;
-            transition: 0.5s;
-            padding-top: 60px;
-            opacity: 75%;
-        }
-        .sidepanel a {
-            padding: 8px 32px 8px 8px;
-            text-decoration: none;
-            /* font-size: 25px; */
-            color: #818181;
-            display: block;
-            transition: 0.3s;
-        }
-        .sidepanel a:hover {
-            color: #f1f1f1;
-        }
-        .sidepanel .closebtn {
-            position: absolute;
-            top: 0;
-            left: 8px;
-            font-size: 36px;
-        }
-        .openbtn {
-            font-size: 20px;
-            cursor: pointer;
-            background-color: rgb(214, 214, 214);
-            color: white;
-            padding: 6px 12px;
-            border: none;
-        }
-        .openbtn:hover {
-            background-color:#444;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
 <nav class="navigation container">
@@ -111,13 +68,11 @@ require_once "config.php";
             $sqlImg = "SELECT * FROM profileimg WHERE userid='$id';";
             $resultImg = mysqli_query($conn, $sqlImg);
             if ($rowImg = mysqli_fetch_assoc($resultImg)) {
-                    echo "<div>";
+                    echo "<div class='user-container'>";
                     if($rowImg['status'] == 0) {
                         echo "<img src='uploads/profile".$id.".jpg'>";
-                        // echo "<img src='uploads/profiledefault.jpg'>";
                     } else {
                         echo "<img src='uploads/profiledefault.jpg'>";
-                        // echo "<img src='uploads/profile".$id.".jpg'>";
                     }
                     echo $row['username'];
                     echo "</div>";
